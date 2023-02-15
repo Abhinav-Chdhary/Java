24e24c34369d64f3e77f098a6a8eb7f955c7b5b2
Abhinav Choudhary <abhinavchdhary@gmail.com>
2023-02-15 14:43:19 +0530

    Create Prime.java

---
+++ CLIinput/Prime.java
@@ -0,0 +1,18 @@
+package CLIinput;
+
+public class Prime {
+    public static void main(String[] args){
+        int n=Integer.parseInt(args[0]);
+        int flag=0;
+        for(int i=0; i<=n/2; i++){
+            if(n%i==0){
+                flag = 1;
+                break;
+            }
+        }
+        if(flag==1)
+            System.out.println("NO not a prime number");
+        else
+            System.out.println("Yes it is a prime number");
+    }
+}
