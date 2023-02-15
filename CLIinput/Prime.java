757394235d44576cd7c8468e262d6f13e2c37246
Abhinav Choudhary <abhinavchdhary@gmail.com>
2023-02-15 14:47:33 +0530

    Prime number

--- CLIinput/Prime.java
+++ CLIinput/Prime.java
@@ -4,7 +4,7 @@ public class Prime {
     public static void main(String[] args){
         int n=Integer.parseInt(args[0]);
         int flag=0;
-        for(int i=0; i<=n/2; i++){
+        for(int i=2; i<=n/2; i++){
             if(n%i==0){
                 flag = 1;
                 break;
