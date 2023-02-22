065b0d2b163cca6c8ca1e484ae763977ab65725a
Abhinav Choudhary <abhinavchdhary@gmail.com>
2023-02-22 14:32:50 +0530

    Create EvenOrOdd.java

---
+++ BasicIO/EvenOrOdd.java
@@ -0,0 +1,17 @@
+package BasicIO;
+
+import java.io.*;
+
+public class EvenOrOdd {
+    public static void main(String args[])throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        System.out.println("Enter a number");
+        int n = Integer.parseInt(br.readLine());
+
+        if(n%2==0)
+            System.out.println("The number is Even");
+        else
+            System.out.println("The number is Odd");
+    }
+}
